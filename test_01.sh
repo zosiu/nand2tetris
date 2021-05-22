9e285cf61a6f9be91efdd2eeb3af1be97ea505ac
zs <baloghzsof@gmail.com>
2021-05-22 05:45:16 +0200

    Boolean logic

---
+++ test_01.sh
@@ -0,0 +1,24 @@
+elementary_gates=(Not And Or Xor Mux DMux)
+variants_16_bit=(Not16 And16 Or16 Mux16)
+variants_multi_way=(Or8Way Mux4Way16 Mux8Way16 DMux4Way DMux8Way)
+
+echo "ELementary logic gates"
+for i in "${elementary_gates[@]}"
+do
+  echo '  '  $i
+  echo '    ' `../tools/HardwareSimulator.sh 01/$i.tst`
+done
+
+echo "16-bit variants"
+for i in "${variants_16_bit[@]}"
+do
+  echo '  '  $i
+  echo '    ' `../tools/HardwareSimulator.sh 01/$i.tst`
+done
+
+echo "Multi-way variants"
+for i in "${variants_multi_way[@]}"
+do
+  echo '  '  $i
+  echo '    ' `../tools/HardwareSimulator.sh 01/$i.tst`
+done
