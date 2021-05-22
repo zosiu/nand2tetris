2f6bf2bc5a29d45f1ed39cf1d5e9887e4711ecf8
zs <baloghzsof@gmail.com>
2021-05-22 19:51:10 +0200

    Boolean Arithmetic, ALU

---
+++ chips/test_02.sh
@@ -0,0 +1,16 @@
+adders=(HalfAdder FullAdder Add16)
+alu=(Inc16 ALU)
+
+echo "Adders"
+for i in "${adders[@]}"
+do
+  echo '  '  $i
+  echo '    ' `../../tools/HardwareSimulator.sh $i.tst`
+done
+
+echo "ALU"
+for i in "${alu[@]}"
+do
+  echo '  '  $i
+  echo '    ' `../../tools/HardwareSimulator.sh $i.tst`
+done
